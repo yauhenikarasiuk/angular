5e9d33ff9ca4af5df49f4ad8385ac96a0078a48d
Yauheni Karasiuk <mr.twing2@gmail.com>
2018-01-07 02:56:39 +0300

    Signed-off-by: Yauheni Karasiuk <mr.twing2@gmail.com>

--- app.js
+++ app.js
@@ -3,5 +3,45 @@ angular.module("myApp",['ngRoute','myApp.page1','myApp.page2'])
 	$routeProvider
 		.otherwise({
 				redirectTo: "/page1"
-		})
-}]);
+		});
+}])
+.factory('SNhttpFactory', function($http){
+	return{
+		get: function(table, params){
+			return $http({
+				url: 'https://dev41098.service-now.com/api/now/table/' + table,
+				method: 'get',
+				headers: {
+					'Accept':'application/json',
+					'Content-Type':'application/json',
+					'Authorization':'Basic ' + btoa('admin'+':'+'49Banimu!'),
+				},
+				params: params
+			});
+		},
+		update: function(table,params,sys_id,data){
+			return $http({
+				url: 'https://dev41098.service-now.com/api/now/table/' + table + '/' + sys_id,
+				method: 'patch',
+				headers: {
+					'Accept':'application/json',
+					'Content-Type':'application/json',
+					'Authorization':'Basic ' + btoa('admin'+':'+'49Banimu!'),
+				},
+				params: params,
+				data: data
+			});
+		},
+		delete: function(table,params,sys_id){
+			return $http({
+				url: 'https://dev41098.service-now.com/api/now/table/' + table + '/' + sys_id,
+				method: 'delete',
+				headers: {
+					'Accept':'application/json',
+					'Content-Type':'application/json',
+					'Authorization':'Basic ' + btoa('admin'+':'+'49Banimu!'),
+				}
+			});
+		},
+	};
+})
--- page1/page1.js
+++ page1/page1.js
@@ -6,6 +6,17 @@ angular.module("myApp.page1",['ngRoute'])
 			controller: "page1Ctrl"
 		})
 }])
-.controller('page1Ctrl', function(){
-	console.log('page1');
+.controller('page1Ctrl', function($scope,$http,SNhttpFactory){
+	$scope.params = {
+		sysparm_display_value: "false"
+	};
+	$scope.send = function(e){
+		e.preventDefault();
+		SNhttpFactory[this.type](this.table,this.params,this.sys_id,this.data)
+		.then(function(response){
+			console.log(response.data.result);
+			$scope.response = JSON.stringify(response.data.result);
+		});
+	};
+
 });
